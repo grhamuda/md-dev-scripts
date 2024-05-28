893d2eb3387f404e4e1942edc4a389e73b31b2d0
Mudawikawi Grahadhuita <mgrahadhuita@traveloka.com>
2024-05-29 02:01:03 +0700

    Add script for deleting redis cache with key pattern

---
+++ Redis/redis_delete_multiple_cache.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+PATTERNS=("PRODUCT_01*" "ITEM_01*")
+REDIS_HOST="<redis_host>"
+REDIS_PORT="<redis_port>"
+for pattern in "${PATTERNS[@]}"
+do
+  for key in $(redis-cli -h $REDIS_HOST -p $REDIS_PORT KEYS "$pattern")
+  do
+    echo "Deleting key: $key"
+    result=$(redis-cli -h $REDIS_HOST -p $REDIS_PORT DEL $key)
+    while [[ "$result" == "MOVED "* ]]
+    do
+      target=$(echo $result | awk '{print $3}')
+      new_host=$(echo $target | cut -d':' -f1)
+      new_port=$(echo $target | cut -d':' -f2)
+      result=$(redis-cli -h $new_host -p $new_port DEL $key)
+    done
+  done
+done
